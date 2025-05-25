88bbc00b6089559b6642ca9d63f329c54810ac91
Rayhan2123 <rayhanmalka02@gmail.com>
2025-05-25 23:47:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,133 @@
-<?php 
-/*Install Midtrans PHP Library (https://github.com/Midtrans/midtrans-php)
-composer require midtrans/midtrans-php
-                              
-Alternatively, if you are not using **Composer**, you can download midtrans-php library 
-(https://github.com/Midtrans/midtrans-php/archive/master.zip), and then require 
-the file manually.   
+document.addEventListener("alpine:init", () => {
+  Alpine.data("products", () => ({
+    items: [
+      { id: 1, name: "Coffee", img: "1.jpg.jpeg", price: 25000 },
+      { id: 2, name: "Copenhagen", img: "2.jpg.jpeg", price: 20000 },
+      { id: 3, name: "Cafe Prohibido", img: "3.jpg.jpeg", price: 38000 },
+      { id: 4, name: "Hause Bland", img: "4.jpg.jpeg", price: 45000 },
+      { id: 5, name: "Naturalist", img: "5.jpg.jpeg", price: 29000 },
+    ],
+  }));
+  Alpine.store("cart", {
+    items: [],
+    total: 0,
+    quantity: 0,
+    add(newItem) {
+      // cek apakah ada barang yang sama di cart
+      const cartItem = this.items.find((item) => item.id === newItem.id);
 
-require_once dirname(__FILE__) . '/pathofproject/Midtrans.php'; */
+      // Jika belum ada / cart masih kosong
+      if (!cartItem) {
+        this.items.push({ ...newItem, quantity: 1, total: newItem.price });
+        this.quantity++;
+        this.total += newItem.price;
+      } else {
+        // jika barang sudah ada,cek apakah barang beda atau sama dengan yang ada dicart
+        this.items = this.items.map((item) => {
+          // jika barang berbeda
+          if (item.id !== newItem.id) {
+            return item;
+          } else {
+            // jika barang sudah ada, tambah quantity dan totalnya
+            item.quantity++;
+            item.total = item.price * item.quantity;
+            this.quantity++;
+            this.total += item.price;
+            return item;
+          }
+        });
+      }
+    },
+    remove(id) {
+      // ambil item yang mau di remove berdasarkan id nya
+      const cartItem = this.items.find((item) => item.id === id);
 
-require_once dirname(__FILE__) . '/midtrans-php-master/Midtrans.php';
+      // jika item lebih dari 1
+      if (cartItem.quantity > 1) {
+        // telusuri 1 1
+        this.items = this.items.map((item) => {
+          // jika bukan barang yang di klik
+          if (item.id !== id) {
+            return item;
+          } else {
+            item.quantity--;
+            item.total = item.price * item.quantity;
+            this.quantity--;
+            this.total -= item.price;
+            return item;
+          }
+        });
+      } else if (cartItem.quantity === 1) {
+        // jika barang sisa 1
+        this.items = this.items.filter((item) => item.id !== id);
+        this.quantity--;
+        this.total -= cartItem.price;
+      }
+    },
+  });
+});
 
-//SAMPLE REQUEST START HERE
+// form validation
+const checkoutButton = document.querySelector(".checkout-button");
+checkoutButton.disabled = true;
 
-// Set your Merchant Server Key
-\Midtrans\Config::$serverKey = 'Mid-server-623YCVwtxIbmpcQdz2ZHoOCf';
-// Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-\Midtrans\Config::$isProduction = true;
-// Set sanitization on (default)
-\Midtrans\Config::$isSanitized = true;
-// Set 3DS transaction for credit card to true
-\Midtrans\Config::$is3ds = true;
+const form = document.querySelector("#checkoutForm");
 
-$params = array(
-    'transaction_details' => array(
-        'order_id' => rand(),
-        'gross_amount' => $_POST['total'],
-    ),
-    'item_detail' => json_decode($_POST['items'], true),
-    'customer_details' => array(
-        'first_name' => $_POST['name'],
-        'email' => $_POST['email'],
-        'phone' => $_POST['phone'],
-    ),
-);
+form.addEventListener("keyup", function () {
+  for (let i = 0; i < form.elements.length; i++) {
+    if (form.elements[i].value.length !== 0) {
+      checkoutButton.classList.remove("disabled");
+      checkoutButton.classList.add("disabled");
+    } else {
+      return false;
+    }
+  }
+  checkoutButton.disabled = false;
+  checkoutButton.classList.remove("disabled");
+});
 
-$snapToken = \Midtrans\Snap::getSnapToken($params);
-echo $snapToken;
+// kirim data ketika tombol checkout diklik
+checkoutButton.addEventListener("click", async function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = new URLSearchParams(formData);
+  const objData = Object.fromEntries(data);
+  // const message = formatMessage(objData);
+  // window.open("http://wa.me/6282195081618?text=" + encodeURIComponent(message));
+
+  // minta transction token menggunakan ajax / fetch
+  try {
+    const response = await fetch("php/placeOrder.php", {
+      method: "POST",
+      body: data,
+    });
+    const token = await response.text();
+    //  console.log(token);
+    window.snap.pay(token);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// format pesan whatsapp
+const formatMessage = (obj) => {
+  return `Data Customer
+  Nama: ${obj.name}
+  Email: ${obj.email}
+  No HP: ${obj.phone}
+  Data Pesanan
+  ${JSON.parse(obj.items).map(
+    (item) => `${item.name} (${item.quantity} x ${rupiah(item.total)}) \n`
+  )}
+  TOTAL:${rupiah(obj.total)}
+  Terima kasih.`;
+};
+
+// Konversi ke Rupiah
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  }).format(number);
+};
